c59cb4249e889144feaa04c6c26dc60e8e83872a
Yann Dirson <ydirson@free.fr>
2021-11-22 20:17:17 +0100

    Include a script to build qubes-vm artifacts: bzImage and modules.img
    ... as support for quick kernel rebuilds

---
+++ qubes-make.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -ex
+
+make -j8 bzImage modules KERNELRELEASE=5.14.15-1.fc32.qubes.x86_64
+rm -rf TMP
+make modules_install INSTALL_MOD_PATH=$PWD/TMP
+ln arch/x86/boot/bzImage TMP/vmlinuz
+ln initramfs TMP/
+PATH="/sbin:$PATH" mkfs.ext3 -d ./TMP -U dcee2318-92bd-47a5-a15d-e79d1412cdce ~/kernel/modules.img 1024M
