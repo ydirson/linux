4b2fca7e4d8fe19157599460b744bc3d853e0bf2
Yann Dirson <ydirson@free.fr>
2021-12-08 12:19:16 +0100

    fixup! Include a script to build qubes-vm artifacts: bzImage and modules.img

--- qubes-make.sh
+++ qubes-make.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 set -ex
 
-MAKE_ARGS="KERNELRELEASE=5.15.4-0.fc32.qubes.x86_64"
-make -j8 $MAKE_ARGS bzImage modules
 rm -rf TMP
-make $MAKE_ARGS modules_install INSTALL_MOD_PATH=$PWD/TMP
-ln arch/x86/boot/bzImage TMP/vmlinuz
-ln initramfs TMP/
-PATH="/sbin:$PATH" mkfs.ext3 -d ./TMP -U dcee2318-92bd-47a5-a15d-e79d1412cdce ~/kernel/modules.img 1024M
+rm -f ~/kernel/modules.img ~/kernel/initramfs ~/kernel/vmlinuz
+
+make "$@" bzImage modules
+
+make "$@" modules_install INSTALL_MOD_PATH=$PWD/TMP
+
+# "sudo dracut" requested by ldconfig and fsfreeze (!?)
+kernelrelease=$(cd TMP/lib/modules && echo *)
+PATH="/sbin:$PATH" sudo dracut -v --nomdadmconf --nolvmconf \
+    --kmoddir TMP/lib/modules/"$kernelrelease" \
+    --modules "kernel-modules qubes-vm-simple" \
+    --conf /dev/null --confdir /var/empty \
+    -d "xenblk xen-blkfront cdrom ext4 jbd2 crc16 dm_snapshot" \
+    TMP/lib/modules/initramfs "$kernelrelease"
+sudo chown $USER TMP/lib/modules/initramfs
+
+ln arch/x86/boot/bzImage TMP/lib/modules/vmlinuz
+ln TMP/lib/modules/vmlinuz TMP/lib/modules/initramfs ~/kernel/
+
+PATH="/sbin:$PATH" mkfs.ext3 -d ./TMP/lib/modules -U dcee2318-92bd-47a5-a15d-e79d1412cdce ~/kernel/modules.img 1024M
