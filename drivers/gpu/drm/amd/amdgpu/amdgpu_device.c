b3cb25a0dba028b5d02ac732d66c64140a7763ea
Yann Dirson <ydirson@free.fr>
2021-12-08 20:41:46 +0100

    fixup! amdgpu: trace init/fini of each IP block to hunt expose bug on psp init failure

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -2241,7 +2241,7 @@ static int amdgpu_device_ip_hw_init_phase1(struct amdgpu_device *adev)
 		if (adev->ip_blocks[i].version->type == AMD_IP_BLOCK_TYPE_COMMON ||
 		    (amdgpu_sriov_vf(adev) && (adev->ip_blocks[i].version->type == AMD_IP_BLOCK_TYPE_PSP)) ||
 		    adev->ip_blocks[i].version->type == AMD_IP_BLOCK_TYPE_IH) {
-			DRM_INFO("hw_init of IP block <%s>...\n",
+			DRM_INFO("hw_init (phase1) of IP block <%s>...\n",
 				 adev->ip_blocks[i].version->funcs->name);
 			r = adev->ip_blocks[i].version->funcs->hw_init(adev);
 			if (r) {
@@ -2265,6 +2265,8 @@ static int amdgpu_device_ip_hw_init_phase2(struct amdgpu_device *adev)
 			continue;
 		if (adev->ip_blocks[i].status.hw)
 			continue;
+		DRM_INFO("hw_init (phase2) of IP block <%s>...\n",
+			 adev->ip_blocks[i].version->funcs->name);
 		r = adev->ip_blocks[i].version->funcs->hw_init(adev);
 		if (r) {
 			DRM_ERROR("hw_init of IP block <%s> failed %d\n",
@@ -2303,6 +2305,8 @@ static int amdgpu_device_fw_loading(struct amdgpu_device *adev)
 					return r;
 				}
 			} else {
+				DRM_INFO("hw_init (fw_loading) of IP block <%s>...\n",
+					 adev->ip_blocks[i].version->funcs->name);
 				r = adev->ip_blocks[i].version->funcs->hw_init(adev);
 				if (r) {
 					DRM_ERROR("hw_init of IP block <%s> failed %d\n",
@@ -2361,7 +2365,7 @@ static int amdgpu_device_ip_init(struct amdgpu_device *adev)
 				DRM_ERROR("amdgpu_vram_scratch_init failed %d\n", r);
 				goto init_failed;
 			}
-			DRM_INFO("hw_init of IP block <%s>...\n",
+			DRM_INFO("hw_init (ip_init) of IP block <%s>...\n",
 				 adev->ip_blocks[i].version->funcs->name);
 			r = adev->ip_blocks[i].version->funcs->hw_init((void *)adev);
 			if (r) {
